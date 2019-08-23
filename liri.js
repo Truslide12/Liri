f8cd41ab22c2dd40cb9ba6bf2d0ad85d485111b4
Carlos Aragon <carlos.s.aragon12@gmail.com>
2019-08-23 10:19:43 -0700

    I removed the displayData and logData functions as I could not get them to work. I added code to console.log all data as a group and log the data to the log.txt file. SpotifyAPI is now having the same issue loading data that it was before. working on that next

--- liri.js
+++ liri.js
@@ -11,38 +11,11 @@ var spotify = new Spotify(keys.spotify);
 
 var moment = require("moment");
 
-var userCommand = process.argv[2]
+var userCommand = process.argv[2];
 //console.log(userCommand);
 
-var userInput = process.argv[3]//.split(" ",3).join("+");
+var userInput = process.argv.splice(3).join("+")
 //console.log(userInput);
-
-// create a new object with information for the information pull
-var artistData = {
-    artist: "",
-    song: "",
-    songName: "",
-    previewLink: "",
-    album: "",
-}
-
-var eventData = {
-    artist: "",
-    venue: "",
-    location: "",
-    date: "",
-}
-
-var movieData = {
-    movie: "",
-    year: "",
-    rating: "",
-    rottenRating: "",
-    country: "",
-    language: "",
-    plot: "",
-    actors: "",
-}
 
 //---------------- Fucntions ------------------//
 var concertThis = (artist) => {
@@ -51,13 +24,30 @@ var concertThis = (artist) => {
     // return name of venue
     // venue location
     // date of the event
-    var queryURL="https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(queryURL)
         .then(function (response) {
-            console.log(response);
-            eventData.venue = concerts[0].venue.name;
-            eventData.location = concerts[0].venue.city;
-            eventData.date = moment(concerts[0].datetime).format('MMMM Do YYYY');
+            var concerts = response.data
+            //console.log(response);
+            if (concerts.length == 0 || concerts === undefined) {
+                console.log("-------------")
+                console.log("Sorry this artist has not been found or not on tour")
+                console.log("-------------")
+
+            } else {
+                console.log('-----------------' + "\n" +
+                    "Venue Name: " + concerts[0].venue.name + "\n" +
+                    "Venue Location: " + concerts[0].venue.city + "\n" +
+                    "Date: " + moment(concerts[0].datetime).format('MMMM Do YYYY'));
+
+                fs.appendFile('./random.txt', '-----------------' + "\n" +
+                    concerts[0].venue.name + "\n" +
+                    concerts[0].venue.city + "\n" +
+                    moment(concerts[0].datetime).format('MMMM Do YYYY') + "\n" +
+                    '-----------------', function (err) {
+                        console.log('Saved!');
+                    });
+            }
         })
         .catch(function (error) {
             console.log(error);
@@ -68,8 +58,6 @@ var concertThis = (artist) => {
                 console.log(error.response.data);
             }
         });
-    // displayData(eventData);
-    // logData(eventData);
 };
 
 var spotifyThisSong = (song) => {
@@ -79,22 +67,22 @@ var spotifyThisSong = (song) => {
     // the song name
     // preview link from spotify
     // the album of the song
-    //console.log(song);
+    console.log(song);
     spotify.search({ type: 'track', query: song }, function (err, data) {
-        var spotifyData = data.tracks.items[0]; 
-        //console.log(spotifyData);
+        var spotifyData = data.tracks.items[0];
+        console.log(spotifyData);
         if (err) {
             console.log(error.response.data);
         } else {
-        //console.log("Artist: " + spotifyData.name);
-        artistData.artist = spotifyData.artists.name;
-        artistData.song = spotifyData.name;
-        artistData.songName = spotifyData.album.name;
-        artistData.previewLink = spotifyData.external_urls;
+            console.log(potifyData.artists.name);
+            console.log("Artist's Name: " + spotifyData.album.artists[0].name);
+            console.log("Song Name: " + spotifyData.name);
+            console.log("Album Name: " + spotifyData.album.name);
+            console.log("URL: " + spotifyData.album.external_urls.spotify);
+            //displayData(artistData),
+            //logData(artistData)
         }
-    })
-    displayData(artistData);
-    logData(artistData);
+    });
 };
 
 var movieThis = (movie) => {
@@ -108,28 +96,35 @@ var movieThis = (movie) => {
    * Actors in the movie.
    -if not movie entered, return "Mr. Nobody" and console.log "Its on Netflix"
 */
-    console.log(omdbKey)
+    //console.log(omdbKey)
     var queryURL = "http://www.omdbapi.com/?t=" + movie + "&apikey=" + omdbKey;
-    console.log(queryURL);
+    //console.log(queryURL);
 
     axios.get(queryURL)
         .then(
             function (response) {
-                console.log(response);
-                // Year the movie came out.
-                movieData.movie = response.data.Title;
-                // IMDB Rating of the movie.
-                movieData.rating = response.data.Rated;
-                // Rotten Tomatoes Rating of the movie.
-                movieData.rottenRating = response.data.Ratings[1].Value; // find correct tag for this
-                // Country where the movie was produced.
-                movieData.country = response.data.Country // also this
-                // Language of the movie.
-                movieData.language = response.data.Language;
-                // Plot of the movie.
-                movieData.plot = response.data.Plot;
-                // Actors in the movie.
-                movieData.actors = response.data.Actors;
+                //console.log(response);
+                console.log('-----------------' + "\n" +
+                    "Title: " + response.data.Title + "\n" +
+                    "Year: " + response.data.Rated + "\n" +
+                    "Rating: " + response.data.Ratings[1].Value + "\n" +
+                    "Rotten Tomato Rating: " + response.data.Country + "\n" +
+                    "Language: " + response.data.Language + "\n" +
+                    "Plot: " + response.data.Plot + "\n" +
+                    "Actors: " + response.data.Actors + "\n" +
+                    '-----------------');
+                console.log("logData Running")
+                fs.appendFile('./random.txt', '-----------------' + "\n" +
+                    "Title: " + response.data.Title + "\n" +
+                    "Year: " + response.data.Rated + "\n" +
+                    "Rating: " + response.data.Ratings[1].Value + "\n" +
+                    "Rotten Tomato Rating: " + response.data.Country + "\n" +
+                    "Language: " + response.data.Language + "\n" +
+                    "Plot: " + response.data.Plot + "\n" +
+                    "Actors: " + response.data.Actors + "\n" +
+                    '-----------------', function (err) {
+                        console.log('Saved!');
+                    });
             })
         .catch(function (error) {
             if (error.response) {
@@ -139,8 +134,6 @@ var movieThis = (movie) => {
                 console.log(error.response.data);
             }
         });
-    displayData(movieData);
-    logData(movieData);
 };
 
 var doWhatItSays = () => {
@@ -155,51 +148,35 @@ var doWhatItSays = () => {
     });
 };
 
+// run user command
+switch (userCommand) {
 
-var displayData = (array) => {
-    console.log("displayData Running")
-    for (var i = 0; i < array.lenth; i++) {
-        console.log(arrayt[i].key + ": " + array[i].element);
-    };
+    case "concert-this":
+        if (userInput === undefined || userInput === null) {
+            console.log("Please enter an artist or band name")
+        }
+        concertThis(userInput);
+        break;
+
+    case "spotify-this":
+        if (userInput === undefined || userInput === null) {
+            uIn = "Ace of Base";
+        }
+        spotifyThisSong(userInput);
+        break;
+
+    case "movie-this":
+        if (userInput === undefined || userInput === null) {
+            userInput = "Mr. Nobody";
+        }
+        console.log("works");
+        movieThis(userInput);
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+
+    default:
+        console.log("Please enter an approprite command")
 }
-var logData = (array) => {
-    console.log("logData Running")
-    for (var i = 0; i < array.lenth; i++) {
-        fs.appendFile('./random.txt', key + ": " + element, function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
-    }
-}
-    // run user command
-    switch (userCommand) {
-
-        case "concert-this":
-            if (userInput === undefined || userInput === null) {
-                console.log("Please enter an artist or band name")
-            }
-            concertThis(userInput);
-            break;
-
-        case "spotify-this":
-            if (userInput === undefined || userInput === null) {
-                uIn = "Ace of Base";
-            }
-            spotifyThisSong(userInput);
-            break;
-
-        case "movie-this":
-            if (userInput === undefined || userInput === null) {
-                userInput = "Mr. Nobody";
-            }
-            console.log("works");
-            movieThis(userInput);
-            break;
-
-        case "do-what-it-says":
-            doWhatItSays();
-            break;
-
-        default:
-            console.log("Please enter an approprite command")
-    }
